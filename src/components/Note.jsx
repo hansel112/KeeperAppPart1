4ab1c846bdd252ae4b22b0018e2236faa2daff91
HANSEL CARZERLET <124737107+hansel112@users.noreply.github.com>
2025-02-07 00:53:10 +0300

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is the note Heading</h1>
+      <p>This is the note content</p>
+      <p>This is a note content</p>
+      <p>This is a note content</p>
+      <p>This is a note content</p>
+      <p>This is a note content</p>
+    </div>
+  );
+}
+
+export default Note;
